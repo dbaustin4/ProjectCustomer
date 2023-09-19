abdc00cc854df32fda32d2e6ea7bfe3f3bcf463f
morqp <mara.cosneanu@yahoo.com>
2023-09-19 14:41:14 +0200

    new assets kinda working

--- Assets/AssetsSampleScene/Scripts/InspectScript.cs
+++ Assets/AssetsSampleScene/Scripts/InspectScript.cs
@@ -3,36 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InspectScript : MonoBehaviour {
+public class InspectScript : MonoBehaviour
+{
 
-  public GameObject[] InspectionObjects; // array with all the vapes
-  private int currIndex; // picks which vape it shows
+    public GameObject[] InspectionObjects; // array with all the vapes
+    private int currIndex; // picks which vape it shows
 
-  [SerializeField]
-  private GameObject exitButton;
+    [SerializeField]
+    private GameObject exitButton;
 
-  void Start() {
-    Debug.Log("hi mara :)");
+    [SerializeField]
+    private AudioClip backSound;
 
-  }
+    private AudioSource audioSource;
 
-  public void Inspect() {
-    Debug.Log("Inspect Executed");
-  }
+    void Start()
+    {
+        Debug.Log("hi mara :)");
+        audioSource = GetComponent<AudioSource>();
 
-  public void Inspect(int index) { //put in which vape u want displayed when u interact
+    }
 
-    Debug.Log("Inspect Executed");
-    currIndex = index; //takes the index and makes it the currrent shown vape
-    InspectionObjects[index].SetActive(true);//makes it active so u can see and rotate it
-  }
+    public void Inspect()
+    {
+        Debug.Log("Inspect Executed");
+    }
 
-  public void TurnOffInspect() {
+    public void Inspect(int index)
+    { //put in which vape u want displayed when u interact
 
-    InspectionObjects[currIndex].SetActive(false); //turns it off bruh
-    exitButton.SetActive(false); //set button to false again
+        Debug.Log("Inspect Executed");
+        currIndex = index; //takes the index and makes it the currrent shown vape
+        InspectionObjects[index].SetActive(true);//makes it active so u can see and rotate it
+    }
 
-  }
+    public void TurnOffInspect()
+    {
+
+        InspectionObjects[currIndex].SetActive(false); //turns it off bruh
+        exitButton.SetActive(false); //set button to false again
+
+    }
+
+    public void BackSound()
+    {
+        audioSource.clip = backSound;
+        audioSource.Play(); //plays the audio
+
+    }
 
 
 }
