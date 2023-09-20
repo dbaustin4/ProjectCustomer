570741353c81426ae94751546b0dc4565a7def8f
dbaustin4 <dbuanahagi4@gmail.com>
2023-09-20 11:49:53 +0200

    GameUI
    Settings and the Canvas for the Game renamed to GameUI

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] GameObject settingsMenu;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
